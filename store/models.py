552bf0f4a9e6414d23ba4f70dc90761be227c488
Sahil Roy <sahilroy2001.sr@gmail.com>
2023-02-28 14:25:57 +0530

    added product gallery

--- store/models.py
+++ store/models.py
@@ -3,22 +3,23 @@ from category.models import Category
 from django.urls import reverse
 from accounts.models import Account
 from django.db.models import Avg, Count
+
 # Create your models here.
 
 class Product(models.Model):
-    product_name = models.CharField(max_length=200, unique=True)
-    slug         = models.SlugField(max_length=200, unique=True)
-    description  = models.TextField(max_length=500, blank=True)
-    price        = models.IntegerField()
-    images       = models.ImageField(upload_to='photos/products', blank=True)
-    stock        = models.IntegerField()
-    is_available = models.BooleanField(default=True)
-    category     = models.ForeignKey(Category, on_delete=models.CASCADE)
-    created_date = models.DateTimeField(auto_now_add=True)
-    modified_date= models.DateTimeField(auto_now=True)
+    product_name    = models.CharField(max_length=200, unique=True)
+    slug            = models.SlugField(max_length=200, unique=True)
+    description     = models.TextField(max_length=500, blank=True)
+    price           = models.IntegerField()
+    images          = models.ImageField(upload_to='photos/products')
+    stock           = models.IntegerField()
+    is_available    = models.BooleanField(default=True)
+    category        = models.ForeignKey(Category, on_delete=models.CASCADE)
+    created_date    = models.DateTimeField(auto_now_add=True)
+    modified_date   = models.DateTimeField(auto_now=True)
 
     def get_url(self):
-        return reverse('product_detail',args=[self.category.slug, self.slug])
+        return reverse('product_detail', args=[self.category.slug, self.slug])
 
     def __str__(self):
         return self.product_name
@@ -48,6 +49,7 @@ variation_category_choice = (
     ('color', 'color'),
     ('size', 'size'),
 )
+
 class Variation(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     variation_category = models.CharField(max_length=100, choices=variation_category_choice)
@@ -57,8 +59,9 @@ class Variation(models.Model):
 
     objects = VariationManager()
 
-    def __unicode__(self):
+    def __str__(self):
         return self.variation_value
+
 
 class ReviewRating(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
@@ -73,3 +76,15 @@ class ReviewRating(models.Model):
 
     def __str__(self):
         return self.subject
+
+
+class ProductGallery(models.Model):
+    product = models.ForeignKey(Product, default=None, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='store/products', max_length=255)
+
+    def __str__(self):
+        return self.product.product_name
+
+    class Meta:
+        verbose_name = 'productgallery'
+        verbose_name_plural = 'product gallery'
--- store/views.py
+++ store/views.py
@@ -1,44 +1,46 @@
-from django.shortcuts import render,get_object_or_404,redirect
-from .models import Product,ReviewRating
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Product, ReviewRating, ProductGallery
 from category.models import Category
 from carts.models import CartItem
+from django.db.models import Q
+
 from carts.views import _cart_id
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.http import HttpResponse
-from django.db.models import Q
 from .forms import ReviewForm
 from django.contrib import messages
 from orders.models import OrderProduct
 
-# Create your views here.
+
 def store(request, category_slug=None):
-    categories=None
-    products=None
-    if category_slug!=None:
-        categories=get_object_or_404(Category,slug=category_slug)
-        products=Product.objects.filter(category=categories, is_available=True)
-        paginator=Paginator(products, 3)
-        page= request.GET.get('page')
-        paged_products= paginator.get_page(page)
-        product_count=products.count()
+    categories = None
+    products = None
+
+    if category_slug != None:
+        categories = get_object_or_404(Category, slug=category_slug)
+        products = Product.objects.filter(category=categories, is_available=True)
+        paginator = Paginator(products, 1)
+        page = request.GET.get('page')
+        paged_products = paginator.get_page(page)
+        product_count = products.count()
     else:
-        products=Product.objects.all().filter(is_available=True).order_by('id')
-        paginator=Paginator(products, 3)
-        page= request.GET.get('page')
-        paged_products= paginator.get_page(page)
-        product_count=products.count()
-    context={
-        'products':paged_products,
-        'product_count':product_count,
+        products = Product.objects.all().filter(is_available=True).order_by('id')
+        paginator = Paginator(products, 3)
+        page = request.GET.get('page')
+        paged_products = paginator.get_page(page)
+        product_count = products.count()
+
+    context = {
+        'products': paged_products,
+        'product_count': product_count,
     }
     return render(request, 'store/store.html', context)
 
-def product_detail(request,category_slug,product_slug):
+
+def product_detail(request, category_slug, product_slug):
     try:
-        single_product=Product.objects.get(category__slug=category_slug, slug=product_slug)
-        in_cart=CartItem.objects.filter(cart__cart_id=_cart_id(request), product=single_product).exists()
-        # return HttpResponse(in_cart)
-        # exit()
+        single_product = Product.objects.get(category__slug=category_slug, slug=product_slug)
+        in_cart = CartItem.objects.filter(cart__cart_id=_cart_id(request), product=single_product).exists()
     except Exception as e:
         raise e
 
@@ -49,15 +51,22 @@ def product_detail(request,category_slug,product_slug):
             orderproduct = None
     else:
         orderproduct = None
+
     # Get the reviews
     reviews = ReviewRating.objects.filter(product_id=single_product.id, status=True)
-    context={
-        'single_product':single_product,
-        'in_cart':in_cart,
+
+    # Get the product gallery
+    product_gallery = ProductGallery.objects.filter(product_id=single_product.id)
+
+    context = {
+        'single_product': single_product,
+        'in_cart'       : in_cart,
         'orderproduct': orderproduct,
         'reviews': reviews,
+        'product_gallery': product_gallery,
     }
-    return render(request, 'store/product_detail.html',context)
+    return render(request, 'store/product_detail.html', context)
+
 
 def search(request):
     if 'keyword' in request.GET:
@@ -70,6 +79,7 @@ def search(request):
         'product_count': product_count,
     }
     return render(request, 'store/store.html', context)
+
 
 def submit_review(request, product_id):
     url = request.META.get('HTTP_REFERER')
