01dafb121dc38982f4b1e0e49e76a51f77cc1344
Sahil Roy <sahilroy2001.sr@gmail.com>
2023-05-06 02:40:32 +0530

    hosting

--- greatkart/urls.py
+++ greatkart/urls.py
@@ -18,6 +18,10 @@ from django.urls import path, include
 from . import views
 from django.conf.urls.static import static
 from django.conf import settings
+
+from django.views.static import serve
+from django.conf.urls import url
+
 urlpatterns = [
     path('admin/', include('admin_honeypot.urls', namespace='admin_honeypot')),
     path('securelogin/', admin.site.urls),
@@ -28,4 +32,7 @@ urlpatterns = [
 
     #ORDERS
     path('orders/', include('orders.urls')),
+
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
