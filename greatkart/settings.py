bdfedb75b481b8c6e0e69d9dde12f689df43b224
Sahil Roy <76893410+sudo-sahilroy@users.noreply.github.com>
2023-05-16 16:21:58 +0530

    Update settings.py

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -147,8 +147,8 @@ MESSAGE_TAGS = {
 }
 
 # SMTP configuration
-EMAIL_HOST = config('EMAIL_HOST')
-EMAIL_PORT = config('EMAIL_PORT', cast=int)
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
-EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'sahilroy2001.sr@gmail.com'
+EMAIL_HOST_PASSWORD = 'bxflersxbqpasqtw'
+EMAIL_USE_TLS = True
